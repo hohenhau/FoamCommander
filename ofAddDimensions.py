c4930ed9846d31f5bb68e17198287d383c7a7385
Alexander von Hohenhau <118744132+hohenhau@users.noreply.github.com>
2025-02-11 18:21:31 +0100

    Update ofAddDimensions.py

--- ofAddDimensions.py
+++ ofAddDimensions.py
@@ -4,6 +4,14 @@
 
 import os
 
+def is_numeric(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+
 def update_dimensions(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
@@ -18,14 +26,21 @@ def update_dimensions(file_path):
                 line = 'dimensions      [0 0 0 0 0 0 0];\n'
             file.write(line)
 
+
 def main():
     target_files = {'yPlus', 'zPlus', 'CourantNumber'}
-    for root, _, files in os.walk('.'):
-        for file_name in files:
-            if file_name in target_files:
-                file_path = os.path.join(root, file_name)
-                update_dimensions(file_path)
-                print(f'Processed file: {file_path}')
+    current_dir = '.'
+
+    for item in os.listdir(current_dir):
+        item_path = os.path.join(current_dir, item)
+        if os.path.isdir(item_path) and is_numeric(item):
+            for root, _, files in os.walk(item_path):
+                for file_name in files:
+                    if file_name in target_files:
+                        file_path = os.path.join(root, file_name)
+                        update_dimensions(file_path)
+                        print(f'Processed file: {file_path}')
+
 
 if __name__ == '__main__':
     main()
