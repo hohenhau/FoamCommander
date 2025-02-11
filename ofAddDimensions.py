70d62f3a57bfe9757f20893a57a4a2619e58e9f7
Alexander von Hohenhau <118744132+hohenhau@users.noreply.github.com>
2025-02-11 19:28:32 +0100

    Update ofAddDimensions.py

--- ofAddDimensions.py
+++ ofAddDimensions.py
@@ -4,6 +4,9 @@
 
 import os
 
+# Global Variables
+DESIRED_FIELDS = {'yPlus', 'Co'}
+
 def is_numeric(string):
     try:
         float(string)
@@ -12,47 +15,49 @@ def is_numeric(string):
         return False
 
 def update_dimensions(file_path):
-    try:
-        with open(file_path, 'rb') as file:
-            lines = []
-            for line in file:
-                try:
-                    decoded_line = line.decode('utf-8')
-                    lines.append(decoded_line)
-                    if 'dimensions' in decoded_line:
-                        lines[-1] = 'dimensions      [0 0 0 0 0 0 0];\n'
-                except UnicodeDecodeError:
-                    print(f"Binary data encountered in {file_path}. Stopping processing.")
-                    return
-        with open(file_path, 'w', encoding='utf-8') as file:
-            file.writelines(lines)
-        print(f'Processed file: {file_path}')
-    except Exception as e:
-        print(f"An error occurred while processing {file_path}: {e}")
+    with open(file_path, 'rb') as file:
+        print(file)
+    # try:
+    #     with open(file_path, 'rb') as file:
+    #         lines = []
+    #         for line in file:
+    #             try:
+    #                 decoded_line = line.decode('utf-8')
+    #                 lines.append(decoded_line)
+    #                 if 'dimensions' in decoded_line:
+    #                     lines[-1] = 'dimensions      [0 0 0 0 0 0 0];\n'
+    #             except UnicodeDecodeError:
+    #                 print(f"Binary data encountered in {file_path}. Stopping processing.")
+    #                 return
+    #     with open(file_path, 'w', encoding='utf-8') as file:
+    #         file.writelines(lines)
+    #     print(f'Processed file: {file_path}')
+    # except Exception as e:
+    #     print(f"An error occurred while processing {file_path}: {e}")
 
-def main():
+
+def obtain_paths_of_relevant_files(desired_fields):
     desired_fields = {'yPlus', 'Co'}
     base_dir = os.getcwd()
     time_steps = [i for i in os.listdir(base_dir) if is_numeric(i)]
     print("Time steps found:", time_steps)
-    relevant_files = list()
+    relevant_paths = list()
     for time_step in time_steps:
         time_dir = os.path.join(base_dir, time_step)
         print(f'Processing directory {time_dir}')
         fields = set([i for i in os.listdir(time_dir)])
         for desired_field in desired_fields:
             if desired_field in fields:
-                relevant_files.append(str(os.path.join(time_dir, desired_field)))
-    for files in relevant_files:
-        print(files)
-        
-        # item_path = os.path.join(current_dir, item)
-        # if os.path.isdir(item_path) and is_numeric(item):
-        #     for root, _, files in os.walk(item_path):
-        #         for file_name in files:
-        #             if file_name in fields:
-        #                 file_path = os.path.join(root, file_name)
-        #                 update_dimensions(file_path)
+                relevant_paths.append(str(os.path.join(time_dir, desired_field)))
+    return relevant_paths
+
+
+def main():
+    global DESIRED_FIELDS
+    relevant_paths = obtain_paths_of_relevant_files(DESIRED_FIELDS)
+    for relevant_path in relevant_paths:
+        update_dimensions(relevant_path) 
+
 
 if __name__ == '__main__':
     main()
