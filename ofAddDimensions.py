721a6862bdda3f695a593ad0a36ba2123492f4c3
Alexander von Hohenhau <118744132+hohenhau@users.noreply.github.com>
2025-02-11 19:19:48 +0100

    Update ofAddDimensions.py

--- ofAddDimensions.py
+++ ofAddDimensions.py
@@ -31,21 +31,18 @@ def update_dimensions(file_path):
         print(f"An error occurred while processing {file_path}: {e}")
 
 def main():
-    fields = {'yPlus', 'Co'}
+    desired_fields = {'yPlus', 'Co'}
     base_dir = os.getcwd()
     time_steps = [i for i in os.listdir(base_dir) if is_numeric(i)]
     print("Time steps found:", time_steps)
     relevant_files = list()
     for time_step in time_steps:
         time_dir = os.path.join(base_dir, time_step)
-        for field in fields:
-            # Collect all files from the directory
-            time_files = [i for i in os.listdir(time_dir)]
-            print(time_files)
-            # Filter out the relevant files using the fields
-            time_files = [i for i in time_files if os.path.isfile(i) and field in i]
-            print(time_files)
-            relevant_files = relevant_files + time_files
+        print(f'Processing directory {time_dir}')
+        fields = set([i for i in os.listdir(time_dir)])
+        for desired_field in desired_fields:
+            if desired_field in fields:
+                relevant_files.append(str(os.path.join(time_dir, desired_field)))
     for files in relevant_files:
         print(files)
         
