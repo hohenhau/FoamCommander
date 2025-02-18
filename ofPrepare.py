deb735f08856a41202e3364b73dc77ea325410ab
Alexander von Hohenhau <118744132+hohenhau@users.noreply.github.com>
2025-02-18 17:39:31 +0100

    Update ofPrepare.py

--- ofPrepare.py
+++ ofPrepare.py
@@ -10,7 +10,7 @@ from ofParseArgs import detect_and_parse_arguments
 PY_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 TEMPLATE_BOUNDARY_DIR = os.path.join(PY_FILE_PATH, "templatesBoundary")
 TEMPLATE_SYSTEM_DIR = os.path.join(PY_FILE_PATH, "templatesSystem")
-CURRENT_DIR = os.getcwd() 
+CURRENT_DIR = os.getcwd()
 TRI_SURFACE_DIR = os.path.join(CURRENT_DIR, "constant", "triSurface")
 ZERO_DIR = os.path.join(CURRENT_DIR, "0.gen")
 SYSTEM_DIR = os.path.join(CURRENT_DIR, "system")
@@ -25,37 +25,66 @@ def initialisation():
     os.makedirs(ZERO_DIR, exist_ok=True)
 
 
-def get_patch_type_from_patch_name(input_name: str):
-    """Determine the patch type based on keywords in the name."""
-    common_types = ['inlet', 'inletOutlet', 'outlet', 'empty', 'symmetry', 'slip', 'cyclicAMI', 'cyclic']
-    additional_types = [('mirror', 'symmetry'),
-                        ('honeycomb', 'honeycomb'),
-                        ('cellSelector', 'cellSelector'),
-                        ('baffle', 'baffle'),
-                        ('porous', 'baffle'),
-                        ('screen', 'baffle'),
-                        ('min', 'empty'),
-                        ('max', 'empty'),
-                        ('atmosphere', 'inletOutlet')]
-    all_types = [(i, i) for i in common_types] + additional_types
-    for patch_name, patch_type in all_types:
-        if patch_name.lower() in input_name.lower():
-            return patch_type
-    return 'wall'
+def get_patch_type_from_patch_name(input_patch_name: str):
+    """Determine the boundary type based the patch name. Needed for: 0/fields (e.g. 0/U)"""
+    # Define the common and additional boundary types (baffle and cellSelector are custom)
+    common_patches = ['inlet', 'outlet', 'cyclic', 'empty', 'movingWallVelocity', 'MRFnoSlip', 'slip', 'symmetry']
+    common_patch_types = {boundary: boundary for boundary in common_patches}
+    common_patch_types['atmosphere'] = 'inletOutlet'
+    common_patch_types['mirror'] = 'symmetry'
+    common_patch_types['min'] = 'empty'
+    common_patch_types['max'] = 'empty'
+    common_patch_types['baffle'] = 'baffle'
+    common_patch_types['porous'] = 'baffle'
+    common_patch_types['screen'] = 'baffle'
+    common_patch_types['cellSelector'] = 'cellSelector'
+    common_patch_types['honeycomb'] = 'cellSelector'
+    # Define types that have overlapping names with the common types
+    overlapping_patch_types = {'cyclicAMI': 'cyclicAMI',             # Overlaps with cyclic
+                               'noSlip': 'noSlip',                   # Overlaps with slip
+                               'symmetryPlane': 'symmetryPlane',     # Overlaps with symmetry
+                               'inletOutlet': 'inletOutlet',         # Overlaps with inlet & outlet
+                               'nonRotating': 'movingWallVelocity'}  # Overlaps with rotating
+    # Convert all KEYS to lower case
+    common_patch_types = {key.lower(): value for key, value in common_patch_types.items()}
+    overlapping_patch_types = {key.lower(): value for key, value in overlapping_patch_types.items()}
+    # Match the patch name to a type (first try common, then overlapping types)
+    input_patch_name = input_patch_name.lower()
+    patch_type = 'wall'
+    for dictionary in [common_patch_types, overlapping_patch_types]:
+        for key, value in dictionary.items():
+            if key in input_patch_name:
+                patch_type = value
+                break
+    return patch_type
 
 
-def get_boundary_type_from_patch_type(input_type: str):
-    """Determine the boundary type based the patch type"""
-    common_boundary_types = ['empty', 'symmetry', 'slip', 'noSlip', 'cyclicAMI', 'cyclic']
-    additional_boundary_types = [('inlet', 'fixedValue'),
-                                 ('outlet', 'zeroGradient'),
-                                 ('wall', 'zeroGradient'),
-                                 ('baffle', 'cyclic')]
-    all_types = [(i, i) for i in common_boundary_types] + additional_boundary_types
-    for patch_type, boundary_type in all_types:
-        if patch_type.lower() in input_type.lower():
-            return boundary_type
-    return 'zeroGradient'
+def get_boundary_type_from_patch_name(input_patch_name: str):
+    """Determine the boundary type based the patch name. Needed for: 0/fields (e.g. 0/U)"""
+    # Define the common and additional boundary types
+    common_boundaries = ['cyclic', 'empty', 'movingWallVelocity', 'MRFnoSlip', 'slip', 'symmetry']
+    common_boundary_types = {boundary: boundary for boundary in common_boundaries}
+    common_boundary_types['inlet'] = 'fixedValue'
+    common_boundary_types['outlet'] = 'zeroGradient'
+    common_boundary_types['wall'] = 'zeroGradient'
+    common_boundary_types['baffle'] = 'cyclic'
+    # Define types that have overlapping names with the common types
+    overlapping_boundary_types = {'noSlip': 'noSlip',                # Overlaps with slip
+                                  'symmetryPlane': 'symmetryPlane',  # Overlaps with symmetry
+                                  'inletOutlet': 'inletOutlet',      # Overlaps with inlet & outlet
+                                  'cyclicAMI': 'cyclicAMI'}          # Overlaps with cyclic
+    # Convert all KEYS to lower case
+    common_boundary_types = {key.lower(): value for key, value in common_boundary_types.items()}
+    overlapping_boundary_types = {key.lower(): value for key, value in overlapping_boundary_types.items()}
+    # Retrieve the patch type from the name and set default boundary type
+    input_patch_type = get_patch_type_from_patch_name(input_patch_name).lower()
+    boundary_type = 'zeroGradient'
+    for dictionary in [common_boundary_types, overlapping_boundary_types]:
+        if input_patch_type in dictionary:
+            boundary_type = dictionary[input_patch_type]
+    if 'ami' in input_patch_name:  # Useful for assigning AMI boundaries that are actually baffles (i.e. baffleAMI)
+        boundary_type = 'cyclicAMI'
+    return boundary_type
 
 
 def load_and_process_stl_files():
@@ -127,20 +156,20 @@ def create_snappy_hex_mesh_dict(patch_names):
         stl_block += f'{" " * 8}{patch}.stl {{type triSurfaceMesh; name {patch}; file "{patch}.stl";}}\n'
         mesh_block += f'{" " * 12}{{file "{patch}.eMesh"; level 3;}}\n'
         patch_type = get_patch_type_from_patch_name(patch)
-        if patch_type == 'baffle':
-            surface_block += f'{" " * 12}{patch} {{level (0 0); faceZone {patch}Faces; }}\n'
-        elif patch_type in {'honeycomb', 'cellSelector'}:
+        if patch_type == 'baffle':   # Options for faceType are {internal, baffle, and boundary}
+            surface_block += f'{" " * 12}{patch} {{level (0 0); faceZone {patch}Faces; faceType baffle;}}\n'
+        elif patch_type == 'cellSelector':
             surface_block += (f'{" " * 12}{patch}\n'
                               f'{" " * 16}{{level (0 0);\n'
                               f'{" " * 16}faceZone {patch}Faces;\n'
                               f'{" " * 16}cellZone {patch}Cells;\n'
                               f'{" " * 16}cellZoneInside inside;}}\n')
-        elif patch_type in {'wall', 'slip', 'empty', 'symmetry'}:
+        elif patch_type in {'wall', 'patch', 'symmetry', 'symmetryPlane', 'empty', 'cyclic', 'cyclicAMI', 'wedge'}:
             surface_block += f'{" " * 12}{patch} {{level (0 0); patchInfo {{type {patch_type};}} }}\n'
-        elif patch_type in {'slip'}:
-            surface_block += f'{" " * 12}{patch} {{level (0 0); patchInfo {{type wall;}} }}\n'
-        else:
+        elif patch_type in {'inlet', 'outlet', 'inletOutlet'}:
             surface_block += f'{" " * 12}{patch} {{level (0 0); patchInfo {{type patch;}} }}\n'
+        else:
+            surface_block += f'{" " * 12}{patch} {{level (0 0); patchInfo {{type wall;}} }}\n'
     # Define the patterns to match the entire lines containing the variables
     patterns_and_replacements = [(r'.*\$STL_FILES_AND_GEOMETRIES\$.*\n', stl_block),
                                  (r'.*\$MESH_FEATURES\$.*\n', mesh_block),
@@ -150,7 +179,7 @@ def create_snappy_hex_mesh_dict(patch_names):
     print(f"snappyHexMeshDict.gen created at: {output_path}")
 
 
-def build_zero_file(names: list, field: str, boundary_types: dict, boundary_vals: dict, internal_val=0):
+def build_zero_file(names: list, field: str, local_boundary_types: dict, boundary_vals: dict, internal_val=0):
     """Creates a file in the zero directory with grouped patch settings."""
     template_path = os.path.join(TEMPLATE_BOUNDARY_DIR, f"{field}")
     output_path = os.path.join(ZERO_DIR, field)
@@ -158,12 +187,12 @@ def build_zero_file(names: list, field: str, boundary_types: dict, boundary_vals
     patch_groups = {}
     for name in names:
         patch_type = get_patch_type_from_patch_name(name)
-        # Overwrite internal cyclicAMI boundaries (i.e. baffleAMI)
-        if 'AMI' in name:
-            patch_type = 'cyclicAMI'
-        # It the patch type is not the same as the boundary type, get the type
-        if patch_type not in boundary_types:
-            boundary_types[patch_type] = get_boundary_type_from_patch_type(patch_type)
+        # Do not add surfaces associated with honeycombs or cell selectors as boundaries
+        if patch_type in {'honeycomb', 'cellSelector'}:
+            return
+        # It the patch type is not specified, get the type
+        if patch_type not in local_boundary_types:
+            local_boundary_types[patch_type] = get_boundary_type_from_patch_name(name)
         # If this is the first patch of its type, start a group
         if patch_type not in patch_groups:
             patch_groups[patch_type] = []
@@ -171,17 +200,14 @@ def build_zero_file(names: list, field: str, boundary_types: dict, boundary_vals
     boundary_block = str()
     # Grouped patches in regex format using pipe separator
     for patch_type, patches in patch_groups.items():
-        # Do not add surfaces associated with honeycombs or cell selectors as boundaries
-        if patch_type in {'honeycomb', 'cellSelector'}:
-            continue
         # Double up cyclic boundaries and baffles
-        if patch_type in {'baffle', 'cyclic'}:
+        if patch_type in {'baffle', 'cyclic', 'cyclicAMI'}:
             patches = [f"{patch}{ending}" for patch in patches for ending in [0, 1]]
         # Join patches with '|' and wrap in parentheses
         patch_group = f'"{patches[0]}"' if len(patches) == 1 else f'"({"|".join(patches)})"'
         # Add the patch text to the text block
         boundary_block += f'    {patch_group}\n    {{\n'
-        boundary_block += f'        type            {boundary_types.get(patch_type, "wall")};\n'
+        boundary_block += f'        type            {local_boundary_types[patch_type]};\n'
         if patch_type in boundary_vals:
             boundary_block += f'        value           {boundary_vals[patch_type]};\n'
         boundary_block += '    }\n'
@@ -197,53 +223,54 @@ def build_zero_file(names: list, field: str, boundary_types: dict, boundary_vals
 
 def create_zero_boundaries(names, fm):
     """Build the zero files for the various fields and boundaries"""
-    
+
     field_configs = {
         'U': {'types': {'wall': 'fixedValue'},
               'values': {'inlet': 'uniform (0 0 0)', 'wall': 'uniform (0 0 0)'},
               'internal_field': 0},
-              
+
         'p': {'types': {'inlet': 'zeroGradient', 'outlet': 'fixedValue'},
               'values': {'outlet': 'uniform 0'},
               'internal_field': 0},
-              
+
         'k': {'types': {'wall': 'kqRWallFunction'},
               'values': {'inlet': '$internalField', 'wall': 'uniform 0'},
               'internal_field': fm.turb_kinetic_energy.value},
-              
+
         'epsilon': {'types': {'wall': 'epsilonWallFunction'},
-                   'values': {'inlet': '$internalField', 'wall': 'uniform 0'},
-                   'internal_field': fm.turb_dissipation_rate.value},
-                   
+                    'values': {'inlet': '$internalField', 'wall': 'uniform 0'},
+                    'internal_field': fm.turb_dissipation_rate.value},
+
         'nut': {'types': {'inlet': 'calculated', 'outlet': 'calculated', 'wall': 'nutUWallFunction'},
                 'values': {'inlet': 'uniform 0', 'outlet': 'uniform 0', 'wall': 'uniform 0'},
                 'internal_field': fm.turb_viscosity.value},
-                
+
         'nuTilda': {'types': {},
                     'values': {'inlet': 'uniform 0'},
                     'internal_field': 0},
-                    
+
         'omega': {'types': {'wall': 'omegaWallFunction'},
-                 'values': {'inlet': '$internalField', 'wall': 'uniform 1e5'},  # value for wall needs to be high (1e5 or 1e6)
-                 'internal_field': fm.specific_dissipation.value},
-                 
+                  'values': {'inlet': '$internalField', 'wall': 'uniform 1e5'},
+                  # value for wall needs to be high (1e5 or 1e6)
+                  'internal_field': fm.specific_dissipation.value},
+
         'kl': {'types': {'wall': 'fixedValue'},
                'values': {'inlet': 'uniform 0', 'wall': 'uniform 0'},
                'internal_field': 0},
-               
+
         'kt': {'types': {'wall': 'fixedValue'},
                'values': {'inlet': 'uniform 0', 'wall': 'uniform 0'},
                'internal_field': 0},
-               
+
         'p_rgh': {'types': {'inlet': 'fixedFluxPressure', 'outlet': 'fixedFluxPressure',
-                           'wall': 'fixedFluxPressure', 'inletOutlet': 'totalPressure'},
-                 'values': {'inlet': 'uniform 0', 'outlet': 'uniform 0',
-                           'wall': 'uniform 0', 'inletOutlet': 'uniform 0'},
-                 'internal_field': 0},
-                 
+                            'wall': 'fixedFluxPressure', 'inletOutlet': 'totalPressure'},
+                  'values': {'inlet': 'uniform 0', 'outlet': 'uniform 0',
+                             'wall': 'uniform 0', 'inletOutlet': 'uniform 0'},
+                  'internal_field': 0},
+
         'alphawatergen': {'types': {'inletOutlet': 'inletOutlet'},
-                         'values': {'inlet': 'uniform 1', 'outlet': 'uniform 0', 'inletOutlet': 'uniform 0'},
-                         'internal_field': 0}
+                          'values': {'inlet': 'uniform 1', 'outlet': 'uniform 0', 'inletOutlet': 'uniform 0'},
+                          'internal_field': 0}
     }
 
     for field_name, config in field_configs.items():
