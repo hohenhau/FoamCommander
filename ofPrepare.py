5e2a7a7cb7f99e090ae19350cfdb5a684005b2f3
Alexander von Hohenhau <118744132+hohenhau@users.noreply.github.com>
2025-02-21 22:42:42 +0100

    Update ofPrepare.py

--- ofPrepare.py
+++ ofPrepare.py
@@ -304,8 +304,8 @@ def create_zero_boundaries(names, fm):
                         f'{" " * 8}type            MRFnoSlip;\n'
                         f'{" " * 8}#else\n'
                         f'{" " * 8}type            movingWallVelocity;\n'
-                        f'{" " * 8}#endif\n'
                         f'{" " * 8}value           uniform (0 0 0);\n'
+                        f'{" " * 8}#endif\n'
                         f'{" " * 4}}}\n')
 
     field_configs = {
