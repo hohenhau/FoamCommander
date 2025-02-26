cea1d347fec1c5665024ecd713448d700cefb8fe
Alexander von Hohenhau <118744132+hohenhau@users.noreply.github.com>
2025-02-26 14:01:09 +0100

    updated ofPrepare

--- ofPrepare.py
+++ ofPrepare.py
@@ -301,7 +301,7 @@ def create_zero_boundaries(names, fm):
 
     field_configs = {
         'U': {'types': {'wall': 'fixedValue', 'MRFnoSlip': 'MRFnoSlip', 'movingWallVelocity': 'movingWallVelocity', 'rotating': rotating_u_types},
-              'values': {'inlet': 'uniform (0 0 0)', 'wall': 'uniform (0 0 0)'},
+              'values': {'inlet': 'uniform (0 0 0)', 'wall': 'uniform (0 0 0)', 'movingWallVelocity': 'uniform (0 0 0)'},
               'internal_field': 0},
 
         'p': {'types': {'inlet': 'zeroGradient', 'outlet': 'fixedValue'},
