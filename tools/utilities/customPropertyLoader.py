1cdd8e44b91a18c20a8d6386411f9c2c5f02e361
Alex <a.hohenha@gmail.com>
2026-01-08 14:55:27 +0100

    Updated how custom properties are accessed and loaded

--- tools/utilities/customPropertyLoader.py
+++ tools/utilities/customPropertyLoader.py
@@ -14,6 +14,7 @@ def load_and_update_custom_properties() -> FlowMetrics:
     # Access and Load custom properties
     file_name = "focoProperties"
     template_file_path = os.path.join(TEMPLATE_CONSTANT_DIR, file_name)
+    print('CHEEEEEEEEEECK')
     check_and_create_file(file_directory=CONSTANT_DIR, file_name=file_name, template_file_path=template_file_path)
     file_path = os.path.join(CONSTANT_DIR, file_name)
     fde = ClassFoamDictEditor(file_path)
