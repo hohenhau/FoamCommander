0ba8986b1c973fa4bf4f1150251bcb1ee7170cc2
Alex <a.hohenha@gmail.com>
2026-01-08 14:45:33 +0100

    Updated how custom properties are accessed and loaded

--- tools/utilities/fileConstants.py
+++ tools/utilities/fileConstants.py
@@ -6,8 +6,8 @@ TEMPLATE_BOUNDARY_DIR = os.path.join(PY_FILE_PATH, "templatesBoundary")
 TEMPLATE_CONSTANT_DIR = os.path.join(PY_FILE_PATH, "templatesConstant")
 TEMPLATE_SYSTEM_DIR = os.path.join(PY_FILE_PATH, "templatesSystem")
 
-CURRENT_DIR = os.getcwd()
-BASE_DIR = os.path.dirname(CURRENT_DIR)
+CWD = os.getcwd()
+BASE_DIR = os.path.dirname(CWD)
 
 TRI_SURFACE_DIR = os.path.join(BASE_DIR, "constant", "triSurface")
 ZERO_DIR = os.path.join(BASE_DIR, "0.gen")
